5a0c0e79e8ab1ab35def76183b16b6a5c8233ee8
fae713 <favourmichael47@gmail.com>
2024-08-21 14:36:05 +0100

    added the secret key directly in the settings file

--- project_hashtags/hashtags_mech/settings.py
+++ project_hashtags/hashtags_mech/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'DJANGO_SECRET_KEY'
+SECRET_KEY = '%+oaxfu$!#j=-5y)c5e82botbqdeo^^7*!ima)brv8p$5qzdyp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
